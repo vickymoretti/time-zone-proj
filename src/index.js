85cf55550377ccf5050b83506a1985076f2f1f6c
victoria <victoria.moretti@hotmail.es>
2023-07-11 15:33:29 +0100

    adding time to the cities in the selector nav

--- src/index.js
+++ src/index.js
@@ -28,53 +28,12 @@ function updateTime() {
 updateTime();
 setInterval(updateTime, 1000);
 
-/*let cities = document.querySelector("#select-city");
+function showSelectedCity(event) {
+	let cityTimeZone = event.target.value;
+	let cityTime = moment().tz(cityTimeZone);
+	let cityElement = document.querySelector(".cities");
+	cityElement.innerHTML = cityTime.format("h:mm:ss [<small>]A[</small>]");
+}
+
+let cities = document.querySelector("#select-city");
 cities.addEventListener("change", showSelectedCity);
-
-let parisTime = moment()
-	.tz("Europe/Paris")
-	.format("dddd, MMMM Do YYYY, h:mm:ss a");
-
-let tokyoTime = moment()
-	.tz("Asia/Tokyo")
-	.format("dddd, MMMM Do YYYY, h:mm:ss a");
-
-let sydneyTime = moment()
-	.tz("Australia/Sydney")
-	.format("dddd, MMMM Do YYYY, h:mm:ss a");
-
-let parisElement = document.querySelector("#paris");
-parisElement.innerHTML = parisTime;
-
-let tokyoElement = document.querySelector("#tokyo");
-tokyoElement.innerHTML = tokyoTime;
-
-let sydneyElement = document.querySelector("#sydney");
-sydneyElement.innerHTML = sydneyTime;
-
-function showSelectedCity(event) {
-	if (event.target.value.length > 0) {
-		if (event.target.value === "paris") {
-			alert(`It is currently ${parisTime}, in Paris`);
-		}
-		if (event.target.value === "tokyo") {
-			alert(`It is currently ${tokyoTime}, in Tokyo`);
-		}
-		if (event.target.value === "sydney") {
-			alert(`It is currently ${sydneyTime}, in Sydney`);
-		}
-	}
-}
-
-/* --- a reduce solution that is not mine ---
-
-function showSelectedCity(event) {
-	if (event.target.value.length > 0) {
-		let currentTime = moment()
-			.tz(event.target.value)
-			.format("dddd, MMMM Do YYYY, h:mm:ss a");
-
-		alert(`it is ${currentTime} in ${event.target.value}`);
-	}
-}
-*/
